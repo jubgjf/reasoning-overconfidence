b9fc99f6c423cb2c981f4000a45cc9a71e73e3b2
jubgjf <guanjiannan@outlook.com>
2025-03-26 23:43:35 +0800

    Support `--no_cot_memory` for reasoning models. Better examination for tokens and string.

--- confidence/extractor.py
+++ confidence/extractor.py
@@ -4,106 +4,97 @@ import re
 from functools import partial
 from typing import Callable
 
-from loguru import logger
-from openai.types.chat import ChatCompletionTokenLogprob
 from result import Err, Ok, Result
 
 from .dataset import DatasetName
-from .method import MethodName
+from .method import MethodName, ResponsePerTurn
 from .utils import first_option_postprocess, gaokao_postprocess, gsm8k_postprocess
 
 
 def extract_answer_and_verbal_confidence(
-    answer_response: str,
-    confidence_response: str | None,
+    question_turn: ResponsePerTurn,
+    confidence_turn: ResponsePerTurn,
     postprocessor: Callable[[str], Result[tuple[str, int, int], str]],
 ) -> Result[tuple[str, float], str]:
-    extracted_result = postprocessor(answer_response)
+    extracted_result = postprocessor(question_turn.answer_content)
     if extracted_result.is_err():
         return extracted_result
     extracted_answer, _, _ = extracted_result.ok_value
 
-    if "</think>" in confidence_response:
-        _, confidence_response_no_thinking = confidence_response.split("</think>")
-    else:
-        confidence_response_no_thinking = confidence_response
-    confidence_score_matches = re.findall(r"\[\[(100|[1-9]?[0-9])]]", confidence_response_no_thinking)
+    confidence_score_matches = re.findall(r"\[\[(100|[1-9]?[0-9])]]", confidence_turn.answer_content)
     if len(confidence_score_matches) < 1:
-        return Err(f"No confidence score found in confidence response: {confidence_response_no_thinking}")
+        return Err(f"No confidence score found in confidence response: {confidence_turn.answer_content}")
     return Ok((extracted_answer, float(confidence_score_matches[0])))
 
 
 def extract_answer_and_logprob_confidence(
-    answer_response: str,
+    dataset_name: DatasetName,
+    question_turn: ResponsePerTurn,
     postprocessor: Callable[[str], Result[tuple[str, int, int], str]],
-    logprobs: list[ChatCompletionTokenLogprob] | None,
 ) -> Result[tuple[str, float], str]:
-    answer_response_from_tokens = "".join([t.token for t in logprobs]).rstrip("<|im_end|>")
-    if answer_response.strip() != answer_response_from_tokens.strip():
-        # Sometimes Unicode chars are divided into multiple tokens, so len(tokens) may not match len(answer)
-        logger.warning("Answer token mismatch:")
-        logger.warning(f"answer = {answer_response.replace('\n', '\\n')}")
-        logger.warning(f"tokens = {answer_response_from_tokens.replace('\n', '\\n')}")
-        if "�" in answer_response_from_tokens:
-            logger.warning("Broken Unicode character (�) found, might be normal")
-        else:
-            logger.warning("Broken Unicode character (�) not found, might be abnormal")
-    extracted_result = postprocessor(answer_response_from_tokens)
+    extracted_result = postprocessor(question_turn.answer_content)
     if extracted_result.is_err():
         return extracted_result
     extracted_answer, answer_char_index, answer_char_len = extracted_result.ok_value
     token_index, char_index = 0, 0
-    for tok in logprobs:
+    for tok in question_turn.answer_logprobs:
         token_index += 1
         char_index += len(tok.token)
         if char_index >= answer_char_index:
             break
     else:
-        return Err(f"Answer token not found in {answer_response_from_tokens}")
+        return Err(f"Answer token not found in {question_turn.answer_content}")
     if char_index == answer_char_index:
         # Token is "A" or "B" etc.
-        answer_tokens = logprobs[token_index : token_index + answer_char_len]
+        answer_tokens = question_turn.answer_logprobs[token_index : token_index + answer_char_len]
+        candidate_answer_tokens = question_turn.answer_logprobs[token_index + 1 : token_index + answer_char_len + 1]
     else:
         # Token is " A" or " B" etc.
-        answer_tokens = logprobs[token_index - 1 : token_index - 1 + answer_char_len]
+        answer_tokens = question_turn.answer_logprobs[token_index - 1 : token_index - 1 + answer_char_len]
+        candidate_answer_tokens = question_turn.answer_logprobs[token_index : token_index + answer_char_len]
     if "".join([t.token.strip() for t in answer_tokens]) != extracted_answer:
-        return Err(f"Answer token mismatch: {''.join([t.token.strip() for t in answer_tokens])} != {extracted_answer}")
+        if "".join([t.token.strip() for t in candidate_answer_tokens]) != extracted_answer:
+            # Sometimes "�" in "".join(tokens), so char_index may be wrong, and tokens may not match answer exactly
+            return Err(
+                f"Answer token mismatch: {''.join([t.token.strip() for t in answer_tokens])} != {extracted_answer}"
+            )
+        answer_tokens = candidate_answer_tokens
     top_logprobs = [t.top_logprobs for t in answer_tokens]
     all_number_pairs = list(itertools.product(*top_logprobs))
     logprobs_unnormalized_unfiltered = {
         "".join([p.token.strip() for p in pair]): sum([p.logprob for p in pair]) for pair in all_number_pairs
     }
-    logprobs_unnormalized = dict(
-        filter(
-            lambda item: item[0].lstrip("-").replace(".", "", 1).isdigit(),
-            logprobs_unnormalized_unfiltered.items(),
+    if dataset_name in [DatasetName.GSM8K]:
+        logprobs_unnormalized = dict(
+            filter(
+                lambda item: item[0].lstrip("-").replace(".", "", 1).isdigit(),
+                logprobs_unnormalized_unfiltered.items(),
+            )
         )
-    )
+    elif dataset_name in [DatasetName.ARC, DatasetName.LogiQA]:
+        logprobs_unnormalized = dict(filter(lambda item: item[0] in "ABCD", logprobs_unnormalized_unfiltered.items()))
+    else:
+        raise NotImplementedError
     logprobs_unnormalized = {k: math.exp(v) for k, v in logprobs_unnormalized.items()}
     logprobs_normalized = {k: v / sum(logprobs_unnormalized.values()) for k, v in logprobs_unnormalized.items()}
     return Ok((extracted_answer, logprobs_normalized[extracted_answer]))
 
 
 def extract_answer_and_p_true_confidence(
-    answer_response: str,
-    confidence_response: str | None,
+    question_turn: ResponsePerTurn,
+    confidence_turn: ResponsePerTurn,
     postprocessor: Callable[[str], Result[tuple[str, int, int], str]],
-    logprobs: list[ChatCompletionTokenLogprob] | None,
 ) -> Result[tuple[str, float], str]:
-    extracted_result = postprocessor(answer_response)
+    extracted_result = postprocessor(question_turn.answer_content)
     if extracted_result.is_err():
         return extracted_result
     extracted_answer, _, _ = extracted_result.ok_value
 
-    if "</think>" in confidence_response:
-        _, confidence_response_no_thinking = confidence_response.split("</think>")
-    else:
-        confidence_response_no_thinking = confidence_response
-    confidence_score_matches = re.findall(r"\[\[([01])]]", confidence_response_no_thinking)
+    confidence_score_matches = re.findall(r"\[\[([01])]]", confidence_turn.answer_content)
     if len(confidence_score_matches) < 1:
-        return Err(f"No confidence score found in confidence response: {confidence_response_no_thinking}")
+        return Err(f"No confidence score found in confidence response: {confidence_turn.answer_content}")
     found_left_quote, found_number, found_right_quote, number_token_index = False, False, False, None
-    for i, tok in enumerate(logprobs):
+    for i, tok in enumerate(confidence_turn.answer_logprobs):
         if tok.token.strip() == "[[":
             found_left_quote = True
         elif tok.token.strip() in ["0", "1"]:
@@ -114,8 +105,8 @@ def extract_answer_and_p_true_confidence(
         if found_left_quote and found_number and found_right_quote and number_token_index is not None:
             break
     else:
-        return Err(f"No confidence score found in confidence response: {confidence_response}")
-    confidence_token = logprobs[number_token_index]
+        return Err(f"No confidence score found in confidence response: {confidence_turn.answer_content}")
+    confidence_token = confidence_turn.answer_logprobs[number_token_index]
     logprobs_unnormalized = {"0": float("-inf"), "1": float("-inf")}
     for top_token in confidence_token.top_logprobs:
         if top_token.token.strip() in logprobs_unnormalized.keys():
@@ -128,12 +119,9 @@ def extract_answer_and_p_true_confidence(
 def extract_answer_and_confidence(
     method_name: MethodName,
     dataset_name: DatasetName,
-    answer_response: str,
-    confidence_response: str | None,
-    logprobs: list[ChatCompletionTokenLogprob] | None,
+    question_turn: ResponsePerTurn,
+    confidence_turn: ResponsePerTurn | None,
 ) -> Result[tuple[str, float], str]:
-    assert method_name.need_logprobs == (logprobs is not None)
-
     postprocess_map = {
         DatasetName.GSM8K: gsm8k_postprocess,
         DatasetName.ARC: first_option_postprocess,
@@ -143,11 +131,9 @@ def extract_answer_and_confidence(
     postprocessor = postprocess_map[dataset_name]
 
     extractor_map = {
-        MethodName.Verbal_0_100: partial(extract_answer_and_verbal_confidence, confidence_response=confidence_response),
-        MethodName.LogProb: partial(extract_answer_and_logprob_confidence, logprobs=logprobs),
-        MethodName.P_True: partial(
-            extract_answer_and_p_true_confidence, confidence_response=confidence_response, logprobs=logprobs
-        ),
+        MethodName.Verbal_0_100: partial(extract_answer_and_verbal_confidence, confidence_turn=confidence_turn),
+        MethodName.LogProb: partial(extract_answer_and_logprob_confidence, dataset_name=dataset_name),
+        MethodName.P_True: partial(extract_answer_and_p_true_confidence, confidence_turn=confidence_turn),
     }
     extractor = extractor_map[method_name]
-    return extractor(answer_response=answer_response, postprocessor=postprocessor)
+    return extractor(question_turn=question_turn, postprocessor=postprocessor)
--- confidence/method.py
+++ confidence/method.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from typing import assert_never
 
+from loguru import logger
 from openai.types.chat import ChatCompletionTokenLogprob
 from pydantic import BaseModel
 from result import Ok, Result
@@ -8,6 +9,7 @@ from result import Ok, Result
 from .data import Data
 from .model import Model
 from .template import Template
+from .utils import split_thinking_answer, split_thinking_answer_logprobs
 
 
 class MethodName(Enum):
@@ -31,21 +33,48 @@ class MethodName(Enum):
         if self == self.Verbal_0_100:
             return "Please rate your confidence in the proposed answer on a scale of 0-100. Give your confidence in format: [[xx]]"
         elif self == self.P_True:
-            return "Please rate your confidence in the proposed answer as either 0 or 1. Give your confidence in format: [[xx]]"
+            return "Please rate your confidence in the proposed answer as either 0 or 1. Give your confidence in format: [[x]]"
         elif self == self.LogProb:
             return None
         else:
             assert_never(self)
 
 
+class ResponsePerTurn(BaseModel):
+    thinking_content: str | None
+    answer_content: str
+    thinking_logprobs: list[ChatCompletionTokenLogprob] | None
+    answer_logprobs: list[ChatCompletionTokenLogprob] | None
+
+
 class Response(BaseModel):
-    messages: list[dict[str, str]]
-    logprobs: list[ChatCompletionTokenLogprob] | None
+    history: list[dict[str, str]]
+    turn_0: ResponsePerTurn
+    turn_1: ResponsePerTurn | None
 
 
 class Method:
     def __init__(self, name: MethodName):
         self._name = name
+
+    @staticmethod
+    def _validate_text_vs_tokens(text: str, tokens: list[ChatCompletionTokenLogprob] | None):
+        if tokens is None:
+            return
+
+        text_from_tokens = "".join([t.token for t in tokens])
+
+        # Try eliminating unicode chars
+        if "÷" in text:
+            text_from_tokens = text_from_tokens.replace("��", "÷")
+        if text != text_from_tokens:
+            logger.warning("Text/token mismatch:")
+            logger.warning(f"Text  = {text.replace('\n', '\\n')}")
+            logger.warning(f"Token = {text_from_tokens.replace('\n', '\\n')}")
+            if "�" in text_from_tokens:
+                logger.warning("Broken Unicode character (�) found, might be normal")
+            else:
+                logger.error("Broken Unicode character (�) not found, might be abnormal")
 
     async def request(
         self,
@@ -54,6 +83,7 @@ class Method:
         template: Template,
         temperature: float = 0,
         max_tokens: int = 16384,
+        no_cot_memory: bool = False,
     ) -> Result[Response, str]:
         # ===== First turn =====
         messages = [{"role": "user", "content": template.prompt(data)}]
@@ -63,11 +93,25 @@ class Method:
         if response_result.is_err():
             return response_result
         messages.append({"role": "assistant", "content": response_result.ok_value.message_content})
+
+        thinking_content, answer_content = split_thinking_answer(response_result.ok_value.message_content)
+        thinking_logprobs, answer_logprobs = split_thinking_answer_logprobs(response_result.ok_value.logprobs_content)
+        self._validate_text_vs_tokens(thinking_content, thinking_logprobs)
+        self._validate_text_vs_tokens(answer_content, answer_logprobs)
+
+        turn_0 = ResponsePerTurn(
+            thinking_content=thinking_content,
+            answer_content=answer_content,
+            thinking_logprobs=thinking_logprobs.copy() if thinking_logprobs is not None else None,
+            answer_logprobs=answer_logprobs.copy() if answer_logprobs is not None else None,
+        )
         if not self._name.need_another_turn:
             assert len(messages) == 2
-            return Ok(Response(messages=messages, logprobs=response_result.ok_value.logprobs_content))
+            return Ok(Response(history=messages, turn_0=turn_0, turn_1=None))
 
         # ===== Second turn (Optional) =====
+        if no_cot_memory:
+            messages[1]["content"] = answer_content
         messages.append({"role": "user", "content": self._name.prompt})
         response_result = await model.request(
             messages=messages, temperature=temperature, max_tokens=max_tokens, logprobs=self._name.need_logprobs
@@ -75,5 +119,17 @@ class Method:
         if response_result.is_err():
             return response_result
         messages.append({"role": "assistant", "content": response_result.ok_value.message_content})
+
+        thinking_content, answer_content = split_thinking_answer(response_result.ok_value.message_content)
+        thinking_logprobs, answer_logprobs = split_thinking_answer_logprobs(response_result.ok_value.logprobs_content)
+        self._validate_text_vs_tokens(thinking_content, thinking_logprobs)
+        self._validate_text_vs_tokens(answer_content, answer_logprobs)
+
+        turn_1 = ResponsePerTurn(
+            thinking_content=thinking_content,
+            answer_content=answer_content,
+            thinking_logprobs=thinking_logprobs.copy() if thinking_logprobs is not None else None,
+            answer_logprobs=answer_logprobs.copy() if answer_logprobs is not None else None,
+        )
         assert len(messages) == 4
-        return Ok(Response(messages=messages, logprobs=response_result.ok_value.logprobs_content))
+        return Ok(Response(history=messages, turn_0=turn_0, turn_1=turn_1))
